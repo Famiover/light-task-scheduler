1fc15dcc3749f9b8031b368151d0ccb709e7ba3a
Famiover <famiover@163.com>
2018-10-30 22:37:25 +0800

    测试多个库提交

--- lts-examples/lts-example-jobclient-springboot/src/main/java/com/github/ltsopensource/example/springboot/JobClientApplication.java
+++ lts-examples/lts-example-jobclient-springboot/src/main/java/com/github/ltsopensource/example/springboot/JobClientApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableJobClient
 @ComponentScan("com.github.ltsopensource.example")
 public class JobClientApplication {
-    //
+    //90
 
     public static void main(String[] args) {
         SpringApplication.run(JobClientApplication.class, args);
